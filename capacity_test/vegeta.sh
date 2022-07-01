15c857258c8ae8a3153e3aa0ae7373cacec36d7d
Jader Dias <52470+JaderDias@users.noreply.github.com>
2022-07-01 17:05:52 +0200

    capacity tests: increase rate from 70/s to 1000/s

--- capacity_test/vegeta.sh
+++ capacity_test/vegeta.sh
@@ -7,9 +7,10 @@ GOPATH=$(go env GOPATH)
 $GOPATH/bin/vegeta attack \
     -targets=vegeta_targets.txt \
     -duration 60s \
-    -rate 70/s \
-    -timeout 10s \
-    -max-workers 2 \
+    -rate 1000/s \
+    -timeout 1s \
+    -workers 50 \
+    -max-workers 50 \
     -header 'Accept: application/json' \
     -header 'Content-Type: application/json' \
     > /tmp/vegeta_results.bin
@@ -19,7 +20,7 @@ cat /tmp/vegeta_results.bin \
     | tee vegeta_report.txt
 cat /tmp/vegeta_results.bin \
     | $GOPATH/bin/vegeta report \
-    -buckets "[0,1ms,2ms,4ms,8ms,16ms,32ms,64ms,82ms,100ms,122ms,149ms,182ms,223ms,272ms,332ms,406ms,496ms,512ms,1s,2s,4s,8s,10s]" \
+    -buckets "[0,500us,1ms,1.5ms,2ms,3ms,4ms,8ms,16ms,32ms,64ms,82ms,100ms,122ms,149ms,182ms,223ms,272ms,332ms,406ms,496ms,512ms,1s]" \
     -type hist[buckets] \
     | tee vegeta_histogram.txt
 cat /tmp/vegeta_results.bin | $GOPATH/bin/vegeta plot > vegeta_plot.html
